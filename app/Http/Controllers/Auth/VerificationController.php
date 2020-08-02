d394b1fa0c9fc396a4a6954f0754438c99ec9abc
中村昌暉 <smith@nakamuramasaakiranoMacBook-Pro.local>
2020-08-02 15:56:22 +0900

    Created email verification methods

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
 use App\Http\Controllers\Controller;
 use Illuminate\Auth\Events\Verified;
 use App\Providers\RouteServiceProvider;
@@ -15,7 +18,6 @@ class VerificationController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
 
@@ -40,5 +42,26 @@ class VerificationController extends Controller
     }
 
     public function resend(Request $request)
-    {}
+    {
+        $this->validate($request, [
+            'email' => ['email', 'required']
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        if(! $user){
+            return response()->json(["errors" => [
+                "email" => "No user could be found with this email address"
+            ]], 422);
+        }
+
+        if($user->hasVerifiedEmail()){
+            return response()->json(["errors" => [
+                "message" => "Email address already verified"
+            ]], 422);
+        }
+
+        $user->sendEmailVerificationNotification();
+
+        return response()->json(['status' => "verification link resent"]);
+    }
 }
