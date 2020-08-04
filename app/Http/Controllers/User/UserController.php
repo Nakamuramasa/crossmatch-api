91628a2a0e0f3f02a2417d7be2107ab061bce047
中村昌暉 <smith@nakamuramasaakiranoMacBook-Pro.local>
2020-08-05 00:09:21 +0900

    Created UserRepository

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -6,24 +6,32 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
+use App\Repositories\Contracts\IUser;
 
 class UserController extends Controller
 {
+    protected $users;
+
+    public function __construct(IUser $users)
+    {
+        $this->users = $users;
+    }
+
     public function index()
     {
-        $users = User::all();
+        $users = $this->users->all();
         return UserResource::collection($users);
     }
 
     public function findById($id)
     {
-        $user = User::where('id', $id)->firstOrFail();
+        $user = $this->users->findOrFail($id);
         return new UserResource($user);
     }
 
     public function findByUsername($username)
     {
-        $user = User::where('username', $username)->get();
+        $user = User::findOrFail($username);
         return new UserResource($user);
     }
 }
