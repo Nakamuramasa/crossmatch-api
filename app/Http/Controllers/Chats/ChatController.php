415341ad027bb76b8e6c94ed09772970c3d6bd72
中村昌暉 <smith@nakamuramasaakiranoMacBook-Pro.local>
2020-08-08 20:59:52 +0900

    Created sendMessage methods to ChatController

--- app/Http/Controllers/Chats/ChatController.php
+++ app/Http/Controllers/Chats/ChatController.php
@@ -2,14 +2,49 @@
 
 namespace App\Http\Controllers\Chats;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\Contracts\IChat;
+use App\Repositories\Contracts\IMessage;
+use App\Http\Resources\MessageResource;
 
 class ChatController extends Controller
 {
+    protected $chats;
+    protected $messages;
+
+    public function __construct(IChat $chats, IMessage $messages)
+    {
+        $this->chats = $chats;
+        $this->messages = $messages;
+    }
+
     public function sendMessage(Request $request)
     {
-        
+        $this->validate($request, [
+            'recipient' => ['required'],
+            'body' => ['required']
+        ]);
+
+        $recipient = $request->recipient;
+        $user = auth()->user();
+        $body = $request->body;
+
+        $chat = $user->getChatWithUser($recipient);
+
+        if(! $chat){
+            $chat = $this->chats->create([]);
+            $this->chats->createParticipants($chat->id, [$user->id, $recipient]);
+        }
+
+        $message = $this->messages->create([
+            'user_id' => $user->id,
+            'chat_id' => $chat->id,
+            'body' => $body,
+            'last_read' => null
+        ]);
+
+        return new MessageResource($message);
     }
 
     public function getUserChats()
@@ -18,6 +53,16 @@ class ChatController extends Controller
     }
 
     public function getChatMessages($id)
+    {
+
+    }
+
+    public function markAsRead()
+    {
+
+    }
+
+    public function destroyMessage()
     {
 
     }
--- app/Repositories/Contracts/IChat.php
+++ app/Repositories/Contracts/IChat.php
@@ -3,4 +3,6 @@
 namespace App\Repositories\Contracts;
 
 interface IChat
-{}
+{
+    public function createParticipants($chatId, array $data);
+}
