bbadb04fffed96d14fda3b9b8a3a824af2203692
中村昌暉 <smith@nakamuramasaakiranoMacBook-Pro.local>
2020-08-11 22:06:05 +0900

    Fixed LoginController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -52,7 +52,7 @@ class LoginController extends Controller
         if($user instanceof MustVerifyEmail && ! $user->hasVerifiedEmail()){
             return response()->json(["errors" => [
                 "verification" => "You need to verify your email account"
-            ]]);
+            ]], 422);
         }
 
         throw ValidationException::withMessages([
